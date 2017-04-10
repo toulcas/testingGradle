38dd72a0e4c467cc7be15218ab4e1e069671305e
SA <SA@gmail.com>
2017-04-10 22:43:49 +0300

    Added tests

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -9,4 +9,9 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+    
+    @Test public void testSomeLibraryMethod2() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
 }
